3cf2bb165e5333005c46d9f3a67a2f6443e65543
tianwei <shentianweipku@gmail.com>
2015-04-08 12:29:00 +0800

    sync

---
+++ src/hello.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include "util.h"
+
+int main()
+{
+    std::cout << distance(4,1) << '\n';
+    return 0;
+}
---
+++ src/util.h
@@ -0,0 +1,4 @@
+double distance(float x1, float x2)
+{
+    return x1 - x2;
+}
