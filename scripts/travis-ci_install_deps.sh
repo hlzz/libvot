131dc41694f095b34457d201f838aba89487b2a9
Tianwei Shen <shentianweipku@gmail.com>
2016-05-30 02:44:32 +0800

    travis

--- scripts/travis-ci_install_deps.sh
+++ scripts/travis-ci_install_deps.sh
@@ -1,4 +1,4 @@
-sudo add-apt-repository "deb http://ppa.launchpad.net/alexei.colin/opencv/ubuntu precise main" -y
+# sudo add-apt-repository "deb http://ppa.launchpad.net/alexei.colin/opencv/ubuntu precise main" -y
 sudo add-apt-repository -y ppa:philip5/extra
 sudo apt-get update -qq
 sudo apt-get -qq install libopencv-dev libopencv-nonfree-dev
