9cf8a131382232e4b26808a3c5b8e89d94ad053e
Tianwei Shen <shentianweipku@gmail.com>
2016-05-30 02:13:20 +0800

    travis-ci

--- scripts/travis-ci_update.sh
+++ scripts/travis-ci_update.sh
@@ -8,7 +8,8 @@ then
 else
     #install a newer cmake since at this time travis only has version 2.8.7
     sudo add-apt-repository --yes ppa:kubuntu-ppa/backports
-    sudo add-apt-repository --yes ppa:xqms/opencv-nonfree
+    sudo add-apt-repository "deb http://ppa.launchpad.net/alexei.colin/opencv/ubuntu precise main" -y
     sudo apt-get update -qq
     sudo apt-get install cmake
+    sudo apt-get -qq install libopencv-dev libopencv-nonfree-dev
 fi
