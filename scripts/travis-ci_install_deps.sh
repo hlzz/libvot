f3e218cf80e1dbe6f62b4c0d86e69984c596437d
Tianwei Shen <shentianweipku@gmail.com>
2016-05-30 02:35:20 +0800

    travis

---
+++ scripts/travis-ci_install_deps.sh
@@ -0,0 +1,2 @@
+sudo apt-get install cmake
+sudo apt-get -qq install libopencv-dev libopencv-nonfree-dev
--- scripts/travis-ci_update.sh
+++ scripts/travis-ci_update.sh
@@ -4,12 +4,10 @@ then
     #cmake v2.8.12 is installed on the mac workers now
     #brew update
     #brew install cmake
-    echo
+    echo "Darwin"
 else
     #install a newer cmake since at this time travis only has version 2.8.7
-    sudo add-apt-repository --yes ppa:kubuntu-ppa/backports
+    sudo add-apt-repository -y ppa:kubuntu-ppa/backports
+    sudo add-apt-repository -y ppa:philip5/extra
     sudo apt-get update -qq
-    sudo apt-get install cmake
-    # sudo add-apt-repository "deb http://ppa.launchpad.net/alexei.colin/opencv/ubuntu precise main" -y
-    sudo apt-get -qq install libopencv-dev libopencv-nonfree-dev
 fi
