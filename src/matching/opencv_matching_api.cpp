69d2976b2e585bc44a84eb6228e7bdfe253b169d
Tianwei Shen <shentianweipku@gmail.com>
2016-06-23 11:19:48 +0800

    forgot to add matching sources

---
+++ src/matching/opencv_matching_api.cpp
@@ -0,0 +1,11 @@
+/** \file opencv_matching_api.cpp
+ *	\brief libvot-side APIs of opencv matching utilities
+ *
+ * 	It contains some matching functions for the use of opencv in libvot
+ */
+// Author: Tianwei Shen <shentianweipku@gmail.com>
+
+#include "opencv_matching_api.h"
+
+#include "utils/global_params.h"
+#include "utils/data_types.h"
