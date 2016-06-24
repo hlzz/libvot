58ee7d3239d8615b76eacb54facac27c9d63a18b
Tianwei Shen <shentianweipku@gmail.com>
2016-06-24 13:43:53 +0800

    replace openmvg sift with libvot_feature

---
+++ test/sift_match_test.cpp
@@ -0,0 +1,15 @@
+// Tianwei Shen, HKUST.
+
+/** \file sift_match_test.cpp
+ *	\brief testing feature matching
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
